58ec18fb6247403b375d3122cce7cac06de18ae9
Damon <damon@mahuika.co.nz>
2017-09-27 15:03:23 +0200

    Comment

--- meteor/index.js
+++ meteor/index.js
@@ -16,7 +16,7 @@ module.exports = {
       }
     ],
     "import/no-absolute-path": "off",  // Because of root imports
-    /* REVISIT: once these issues are fixed:
+    /* REVISIT: remove "no-extraneous-dependencies: off" once these issues are fixed:
      https://github.com/benmosher/eslint-plugin-import/issues/479
      https://github.com/clayne11/eslint-import-resolver-meteor/issues/11 */
     "import/no-extraneous-dependencies": "off"
