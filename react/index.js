1191967745f4035ece261e0475b0a6951f01ea81
damon <damon@mahuika.co.nz>
2018-07-04 10:07:51 -0400

    "react/jsx-filename-extension": "off",  // Since crate-react-app doesn't compile .jsx with babel

--- react/index.js
+++ react/index.js
@@ -5,6 +5,7 @@ module.exports = {
   ],
   rules: {
     "react/prop-types": "off",
+    "react/jsx-filename-extension": "off",  // Since crate-react-app doesn't compile .jsx with babel
 
     // Disable formatting rules because we use Prettier
     "react/jsx-wrap-multilines": "off",
